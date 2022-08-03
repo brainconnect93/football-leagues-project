805b0ee5d2850a88442309e5b6072e33a8ff8f11
brainconnect93 <brainconnect0@gmail.com>
2022-08-03 23:53:49 +0100

    Add the character.js file to the component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/Home';
+import Character from './components/Character';
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/teams" element={<Character />} />
     </Routes>
   </BrowserRouter>
 );
---
+++ src/components/Character.js
@@ -0,0 +1,7 @@
+const Character = () => (
+  <div>
+    <h1>Hello Welcome to the football league</h1>
+  </div>
+);
+
+export default Character;
