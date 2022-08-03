6c9bdcbcad56115da7265fcc7b926bf9afb13ddb
brainconnect93 <brainconnect0@gmail.com>
2022-08-03 08:49:53 +0100

    Modify the app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-function App() {
-  return (
-    null
-  );
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route></Route>
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App;
