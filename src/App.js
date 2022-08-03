2719428eac9dad352a5b17a2f1090f68ec4740cb
brainconnect93 <brainconnect0@gmail.com>
2022-08-03 20:00:35 +0100

    Add the homepage link to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import HomePage from './components/Home';
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route></Route>
+      <Route path="/" element={<HomePage />} />
     </Routes>
   </BrowserRouter>
-)
+);
 
 export default App;
